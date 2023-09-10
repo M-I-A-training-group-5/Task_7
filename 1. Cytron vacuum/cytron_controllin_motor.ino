60636c787f71bb65f99df8ff0dd7855c3708ee4c
Ziad Mohamed <141491853+ZiadMohamedElsayed@users.noreply.github.com>
2023-09-10 04:37:21 +0300

    start filter func

--- 1. Cytron vacuum/cytron_controllin_motor.ino
+++ 1. Cytron vacuum/cytron_controllin_motor.ino
@@ -1,25 +1,55 @@
 const int motorPWM = 9;       // PWM pin for motor speed control
 const int motorDirection = 8; // Digital pin for motor direction control
 
-void setup() {
+int currentSpeed = 0;
+int targetSpeed = 0;
+
+void setup()
+{
   pinMode(motorPWM, OUTPUT);       // Set motorPWM as output
   pinMode(motorDirection, OUTPUT); // Set motorDirection as output
+
   Serial.begin(9600); // Initialize serial communication
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    int speed = Serial.parseInt();
-    controlMotor(speed);
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    currentSpeed = targetSpeed;
+    targetSpeed = Serial.parseInt();
+    controlMotor();
   }
 }
 
-void controlMotor(int speed) {
-  if (speed < 0) {
+void controlMotor()
+{
+  if (targetSpeed < 0)
+  {
     digitalWrite(motorDirection, LOW);
-    analogWrite(motorPWM, -1 * speed);
-  } else {
+    targetSpeed = -targetSpeed;
+    startFilter();
+  }
+  else
+  {
     digitalWrite(motorDirection, HIGH);
-    analogWrite(motorPWM, speed);
+    startFilter();
   }
 }
+
+void startFilter()
+{
+  double currentValue = currentSpeed;
+  double targetValue = targetSpeed;
+  double alpha = 0.04;
+
+  while (currentValue < targetValue - 0.1) // Remove this
+  {
+    currentValue = (1.0 - alpha) * (double)currentValue + alpha * (double)targetValue;
+    analogWrite(motorPWM, currentValue);
+    Serial.println(currentValue);
+    delay(20); // Remove this
+  }
+  currentSpeed = targetValue;
+}
+// implement delay to start filter func instead of delay(20)
