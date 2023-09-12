d6128c7f3f7fd972645efb11620ca51f62093a2f
Aly El Din <eng.alyeldin@gmail.com>
2023-09-12 13:51:40 +0300

    arduino code

---
+++ PID_positional_controller.ino
@@ -0,0 +1,94 @@
+#define pinEncoderA 2 
+#define pinEncoderB 3
+
+int position=0;
+int angle=0;
+
+const int motorPWM = 9;       // PWM pin for motor speed control
+const int motorDirection = 8; // Digital pin for motor direction control
+
+long previous_time=0;
+float previous_error=0,eintegral=0;
+
+void controlMotor(int speed) {
+  if (speed < 0) {
+    //motor Rotates in clockwise direction
+    digitalWrite(motorDirection, LOW);
+    //assign speed to the motor (must be +ve)
+    analogWrite(motorPWM, -1 * speed);
+  } else {
+    //motor Rotates in anticlockwise direction
+    digitalWrite(motorDirection, HIGH);
+    //assign speed to the motor
+    analogWrite(motorPWM, speed);
+  }
+}
+
+void setup() {
+  pinMode(pinEncoderA, INPUT_PULLUP);
+  pinMode(pinEncoderB, INPUT_PULLUP);
+
+  attachInterrupt(pinEncoderA, ISR_rotateClockWise, CHANGE);
+  attachInterrupt(pinEncoderB, ISR_rotateAntiClockWise, CHANGE);
+
+  pinMode(motorPWM, OUTPUT);       // Set motorPWM as output
+  pinMode(motorDirection, OUTPUT); // Set motorDirection as output
+  Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+    //set target position
+    int target=1200;
+    float Kp=1,Kd=0,Ki=0;
+
+    long current_time=millis();
+    
+    float deltaT=(float(current_time-previous_time))/1000.0;
+    previous_time=current_time;
+
+    int error=target-position;
+
+    float dervT=(error-previous_error)/deltaT;
+
+    eintegral+=error*deltaT;
+
+    float control_signal=Kp*error+Kd*dervT+Ki*eintegral;
+
+    //motor power
+    controlMotor(control_signal);
+
+    previous_error=error;
+
+    Serial.print(target);
+    Serial.print(" ");
+    Serial.print(position);
+    Serial.println();
+
+}
+
+void ISR_rotateClockWise(void){
+	if(digitalRead(pinEncoderA)!=digitalRead(pinEncoderB))
+      position++;
+  else
+      position--;
+    //clockwise
+}
+ void ISR_rotateAntiClockWise(void){
+ 	if(digitalRead(pinEncoderA)== digitalRead(pinEncoderB))
+      position++;
+   	else
+      position--;
+}
+void controlMotor(float speed) {
+  if (speed < 0) {
+    //motor Rotates in clockwise direction
+    digitalWrite(motorDirection, LOW);
+    //assign speed to the motor (must be +ve)
+    analogWrite(motorPWM, min(-1 * speed,255));
+  } else {
+    //motor Rotates in anticlockwise direction
+    digitalWrite(motorDirection, HIGH);
+    //assign speed to the motor
+    analogWrite(motorPWM, min(speed,255));
+  }
+}
